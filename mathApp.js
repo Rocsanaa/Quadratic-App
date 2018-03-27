32e5a43e81f6729f59d10f8c7b11e22a38f9e4bf
Rocsana Deleon <31667376+Rocsanaa@users.noreply.github.com>
2018-03-26 23:08:51 -0700

    Adjusting by a lot

--- mathApp.js
+++ mathApp.js
@@ -1,29 +1,37 @@
 var button = document.getElementById('solve');
 
 function equation(a, b, c) {
-	var result1 = -b - Math.sqrt(Math.pow(b, 2) - (4 * a * c)) / (2 * a);
-	var result2 = -b + Math.sqrt(Math.pow(b, 2) - (4 * a * c)) / (2 * a);
+    var testP = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    var testN = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    return [testP, testN];
 }
 
-function displaysolve() {
-var a = document.getElementById('a').value;
-var b = document.getElementById('b').value;
-var c = document.getElementById('c').value;
-var input = document.getElementById('inputNum').value;
-    var result = equation(input);
-    var message = document.getElementById('result1');
-    if (isNaN(input)) {
-        message.textContent = "The value " + input + " is not a number.";
-        changeElementClass("inputNum", "invalid");
-    } else if (result == false) {
-        message.textContent = "This is not a prime number.";
-        changeElementClass("inputNum", "fail");
+function displayEquation() {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    var c = document.getElementById('c').value;
+    var result = equation(a, b, c);
+    var resultP = result[0];
+    var resultN = result[1];
+    var message = document.getElementById('solutions');
+    if (isNaN(a) || isNaN(b) || isNaN(c) || a == 0) {
+        message.textContent = "You entered an incorrect value for either a, b, or c. Please try again.";
+        changeElementClass("a", "invalid");
+        changeElementClass("b", "invalid");
+        changeElementCass("c", "invalid");
+
+
+    } else if (isNaN(resultP) || isNan(resultN)) {
+        message.textContent = "There are no real solutions.";
+        changeElementClass("a", "no_real");
+        changeElementClass("b", "no_real");
+        changeElementClass("c", "no_real");
+
     } else {
-        message.textContent = "This is a prime number.";
-        changeElementClass("inputNum", "success");
-    }
-}
+        message.textContent = "The roots to the quadratic are " + resultP + " and " + resultN + ".";
+        changeElementClass("a", "real");
+        changeElementClass("b", "real");
+        changeElementClass("c", "real");
 
-button.addEventListener("click", () => {
-    document.getElementById('result').textContent = solve();
-});
+    }
+   button.addEventListener('click', displayEquation);
